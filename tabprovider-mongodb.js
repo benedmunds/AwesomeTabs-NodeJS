fd78532567fbb798b3c51b2922f7f45a3cdaf5bb
Ben Edmunds <ben.edmunds@gmail.com>
2012-04-05 22:37:55 -0400

    Testing mongohq connection settings

--- tabprovider-mongodb.js
+++ tabprovider-mongodb.js
@@ -5,7 +5,7 @@ var BSON = require('mongodb').BSON;
 var ObjectID = require('mongodb').ObjectID;
 
 TabProvider = function(host, port) {
-  this.db= new Db('awesometabs', new Server(host, port, {auto_reconnect: true}, {}));
+  this.db= new Db('awesometabs', new Server((process.env.MONGOHQ_URL || host), port, {auto_reconnect: true}, {}));
   this.db.open(function(){});
 };
 
